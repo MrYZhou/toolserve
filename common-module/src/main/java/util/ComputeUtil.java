ef602ca151e5c0c4d11e1a5f53641c6c07444737
larry <yinmaisoft.com>
2022-04-08 18:06:09 +0800

    增加middleware-module 和file-module

--- common-module/src/main/java/util/ComputeUtil.java
+++ common-module/src/main/java/util/ComputeUtil.java
@@ -1,7 +1,12 @@
-package BaseUtil;
+package util;
 
 public class ComputeUtil {
-
+    /**
+     * 计算平均值
+     * @param x
+     * @param y
+     * @return
+     */
     public static int get(int x,int y){
      return (x & y) + ((x ^ y) >> 1);
     }
