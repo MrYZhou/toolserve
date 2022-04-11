e9eb477499a2359ce9eb7b7984b6a63192df6737
larry <1762861715@qq.com>
2022-04-11 23:45:37 +0800

    发送email

---
+++ common-module/src/main/java/common/util/EmailTask.java
@@ -0,0 +1,63 @@
+package common.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+
+//@Component
+public class EmailTask {
+    @Autowired
+    MailSender mailSender;
+    @Autowired
+    SimpleMailMessage simpleMailMessage;
+
+//    @Autowired
+//    MimeMessageHelper mimeMessageHelper;
+//
+//    @Autowired
+//    MimeMailMessage mimeMailMessage;
+
+    /**
+     * 普通文字
+     * @param title
+     * @param text
+     * @param from
+     * @param to
+     * @return
+     */
+    public boolean sendMessge(String title,String text,String from,String to){
+
+
+        simpleMailMessage.setSubject(title);
+        simpleMailMessage.setText(text);
+        simpleMailMessage.setBcc();
+        simpleMailMessage.setFrom("1762861715@qq.com");
+        simpleMailMessage.setTo("2271952106@qq.com");
+//        simpleMailMessage.setFrom("1762861715@qq.com");
+//        simpleMailMessage.setTo("2271952106@qq.com");
+        mailSender.send(simpleMailMessage);
+        return true;
+    }
+    public boolean sendMessge(String title,String text,String from){
+
+
+        simpleMailMessage.setSubject(title);
+        simpleMailMessage.setText(text);
+        simpleMailMessage.setSentDate(new Date());
+        simpleMailMessage.setFrom("1762861715@qq.com");
+        simpleMailMessage.setTo("2271952106@qq.com");
+//        simpleMailMessage.setFrom("1762861715@qq.com");
+//        simpleMailMessage.setTo("2271952106@qq.com");
+        mailSender.send(simpleMailMessage);
+        return true;
+    }
+
+
+}
--- main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
+++ main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
@@ -1,6 +1,8 @@
 package com.lar.main;
 
+import common.util.EmailTask;
 import common.util.RedisUtil;
+import middle.task.email.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,14 +11,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MainModuleApplicationTests {
     @Autowired
     RedisUtil redisUtil;
+//    @Autowired
+    EmailTask emailTask;
+
+    @Autowired
+    private MailService mailService;
+
     @Test
-    void contextLoads() {
+    void redisTest() {
         redisUtil.set("testutil","success");
     }
 
+
+
+    @Test
+    void emailTest() {
+
+        mailService.sendSimpleTextMailActual("发送主题","发送内容",new String[]{"2271952106@qq.com"},null,null,null);
+
+    }
+
+
     @Test
     void getload() {
-        System.out.println(redisUtil.get("testutil"));
+        emailTask.sendMessge("主题","hello","1762861715@qq.com","2271952106@qq.com");
     }
 
 }
