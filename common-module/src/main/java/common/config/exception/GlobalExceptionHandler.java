2110be5877faba0570ccf5cbe6532a04d85eabf7
larry <yinmaisoft.com>
2022-04-11 15:23:41 +0800

    增加全局异常和状态码封装,添加redis工具类

---
+++ common-module/src/main/java/common/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package common.config.exception;
+
+import common.base.AppResult;
+import com.alibaba.fastjson.JSON;
+import common.enums.AppResultCode;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseBody
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public AppResult methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> map = new HashMap<>(16);
+        String message = e.getMessage();
+        String json = JSON.toJSONString(message);
+        AppResult result = AppResult.fail(AppResultCode.ValidateError.getCode(), json);
+        return result;
+    }
+}
