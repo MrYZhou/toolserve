cf19b9ba39afa751f1279eb0755d2f68a47200a3
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-04-11 23:45:37 +0800

    发送email

---
+++ common-module/src/main/java/common/config/MailConfig.java
@@ -0,0 +1,36 @@
+package common.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+//@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Bean
+    public SimpleMailMessage getSimpleMailMessage(){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        return simpleMailMessage;
+    }
+
+    @Bean
+    public MailSender getMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        return javaMailSender;
+    }
+
+}
