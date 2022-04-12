3892780cd4d219819c5e2cc555d912e66f735ccd
larry <1762861715@qq.com>
2022-04-12 23:54:34 +0800

    quartz定时

--- common-module/src/main/java/common/util/EmailTask.java
+++ common-module/src/main/java/common/util/EmailTask.java
@@ -11,7 +11,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
-//@Component
+@Component
 public class EmailTask {
     @Autowired
     MailSender mailSender;
--- main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
+++ main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
@@ -2,20 +2,52 @@ package com.lar.main;
 
 import common.util.EmailTask;
 import common.util.RedisUtil;
+import middle.quartz.QuartzJob;
 import middle.task.email.MailService;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 
 @SpringBootTest
 class MainModuleApplicationTests {
     @Autowired
     RedisUtil redisUtil;
-//    @Autowired
+    @Autowired
     EmailTask emailTask;
+
+
 
     @Autowired
     private MailService mailService;
+
+    @Autowired
+    Scheduler scheduler;
+
+    @Test
+    void jobTest() throws SchedulerException {
+        JobDetail job54 = scheduler.getJobDetail(JobKey.jobKey("job54"));
+        if(job54==null){
+            job54 = JobBuilder.newJob(QuartzJob.class).
+                    withIdentity("job54")
+                    .build();
+        }
+
+        Trigger trigger14322 = scheduler.getTrigger(TriggerKey.triggerKey("trigger14322"));
+        if(trigger14322==null){
+            trigger14322 = TriggerBuilder.newTrigger()
+                    .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+                    .withIdentity("trigger14322")
+//                .forJob(jobDetail)
+                    .build();
+        }
+
+        scheduler.scheduleJob(job54,trigger14322);
+        scheduler.start();
+    }
+
 
     @Test
     void redisTest() {
@@ -36,5 +68,6 @@ class MainModuleApplicationTests {
     void getload() {
         emailTask.sendMessge("主题","hello","1762861715@qq.com","2271952106@qq.com");
     }
+
 
 }
