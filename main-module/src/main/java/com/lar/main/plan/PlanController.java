b7fb91627c6df5a9fab6a5ba7bad135000627d38
lingen <yinmaisoft.com>
2022-04-12 13:32:07 +0800

    异步邮件任务

--- main-module/src/main/java/com/lar/main/plan/PlanController.java
+++ main-module/src/main/java/com/lar/main/plan/PlanController.java
@@ -2,6 +2,7 @@ package com.lar.main.plan;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import common.util.ComputeUtil;
 
--- middleware-module/src/main/java/middle/task/email/MailerServiceImpl.java
+++ middleware-module/src/main/java/middle/task/email/MailerServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 
@@ -38,45 +39,46 @@ public class MailerServiceImpl implements MailService {
     private JavaMailSender mailSender;
 
     @Override
-    public void sendSimpleTextMailActual(String subject,String content,String[] toWho,String[] ccPeoples,String[] bccPeoples,String[] attachments){
+    @Async
+    public void sendSimpleTextMailActual(String subject, String content, String[] toWho, String[] ccPeoples, String[] bccPeoples, String[] attachments) {
         //检验参数：邮件主题、收件人、邮件内容必须不为空才能够保证基本的逻辑执行
-        if(subject == null||toWho == null||toWho.length == 0||content == null){
-            log.error("邮件-> {} 无法继续执行，因为缺少基本的参数：邮件主题、收件人、邮件内容",subject);
+        if (subject == null || toWho == null || toWho.length == 0 || content == null) {
+            log.error("邮件-> {} 无法继续执行，因为缺少基本的参数：邮件主题、收件人、邮件内容", subject);
             throw new RuntimeException("模板邮件无法继续发送，因为缺少必要的参数！");
         }
-        log.info("开始发送简单文本邮件：主题->{}，收件人->{}，抄送人->{}，密送人->{}，附件->{}",subject,toWho,ccPeoples,bccPeoples,attachments);
+        log.info("开始发送简单文本邮件：主题->{}，收件人->{}，抄送人->{}，密送人->{}，附件->{}", subject, toWho, ccPeoples, bccPeoples, attachments);
 
         //附件处理，需要处理附件时，需要使用二进制信息，使用 MimeMessage 类来进行处理
-        if(attachments != null&&attachments.length > 0){
-            try{
+        if (attachments != null && attachments.length > 0) {
+            try {
                 //附件处理需要进行二进制传输
                 MimeMessage mimeMessage = mailSender.createMimeMessage();
-                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true,DEFAULT_ENCODING);
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, DEFAULT_ENCODING);
                 //设置邮件的基本信息：这些函数都会在后面列出来
-                boolean continueProcess = handleBasicInfo(helper,subject,content,toWho,ccPeoples,bccPeoples,false);
+                boolean continueProcess = handleBasicInfo(helper, subject, content, toWho, ccPeoples, bccPeoples, false);
                 //如果处理基本信息出现错误
-                if(!continueProcess){
-                    log.error("邮件基本信息出错: 主题->{}",subject);
+                if (!continueProcess) {
+                    log.error("邮件基本信息出错: 主题->{}", subject);
                     return;
                 }
                 //处理附件
-                handleAttachment(helper,subject,attachments);
+                handleAttachment(helper, subject, attachments);
                 //发送该邮件
                 mailSender.send(mimeMessage);
-                log.info("发送邮件成功: 主题->{}",subject);
+                log.info("发送邮件成功: 主题->{}", subject);
             } catch (MessagingException e) {
                 e.printStackTrace();
-                log.error("发送邮件失败: 主题->{}",subject);
+                log.error("发送邮件失败: 主题->{}", subject);
 
             }
-        }else{
+        } else {
             //创建一个简单邮件信息对象
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
             //设置邮件的基本信息
-            handleBasicInfo(simpleMailMessage,subject,content,toWho,ccPeoples,bccPeoples);
+            handleBasicInfo(simpleMailMessage, subject, content, toWho, ccPeoples, bccPeoples);
             //发送邮件
             mailSender.send(simpleMailMessage);
-            log.info("发送邮件成功: 主题->{}",subject,toWho,ccPeoples,bccPeoples,attachments);
+            log.info("发送邮件成功: 主题->{}", subject, toWho, ccPeoples, bccPeoples, attachments);
         }
     }
 
@@ -84,58 +86,58 @@ public class MailerServiceImpl implements MailService {
     public void sendHtmlMail(String subject, String content, String[] toWho) {
 
         //检验参数：邮件主题、收件人、邮件内容必须不为空才能够保证基本的逻辑执行
-        if(subject == null||toWho == null||toWho.length == 0||content == null){
-            log.error("邮件-> {} 无法继续执行，因为缺少基本的参数：邮件主题、收件人、邮件内容",subject);
+        if (subject == null || toWho == null || toWho.length == 0 || content == null) {
+            log.error("邮件-> {} 无法继续执行，因为缺少基本的参数：邮件主题、收件人、邮件内容", subject);
             throw new RuntimeException("模板邮件无法继续发送，因为缺少必要的参数！");
         }
-        log.info("开始发送Html邮件：主题->{}，收件人->{}",subject,toWho);
+        log.info("开始发送Html邮件：主题->{}，收件人->{}", subject, toWho);
         //html
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true,DEFAULT_ENCODING);
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, DEFAULT_ENCODING);
             //设置邮件的基本信息
-            handleBasicInfo(helper,subject,content,toWho);
+            handleBasicInfo(helper, subject, content, toWho);
             //发送邮件
             mailSender.send(mimeMessage);
             log.info("html邮件发送成功");
         } catch (MessagingException e) {
-            log.error("发送邮件出错->{}",subject);
+            log.error("发送邮件出错->{}", subject);
         }
-        log.info("发送邮件成功: 主题->{}",subject,toWho);
+        log.info("发送邮件成功: 主题->{}", subject, toWho);
     }
 
     @Override
-    public boolean handleBasicInfo(MimeMessageHelper mimeMessageHelper,String subject,String content,String[] toWho,String[] ccPeoples,String[] bccPeoples,boolean isHtml){
-        try{
+    public boolean handleBasicInfo(MimeMessageHelper mimeMessageHelper, String subject, String content, String[] toWho, String[] ccPeoples, String[] bccPeoples, boolean isHtml) {
+        try {
             //设置必要的邮件元素
             //设置发件人
-            mimeMessageHelper.setFrom(nickname+'<'+userName+'>');
+            mimeMessageHelper.setFrom(nickname + '<' + userName + '>');
             //设置邮件的主题
             mimeMessageHelper.setSubject(subject);
             //设置邮件的内容，区别是否是HTML邮件
-            mimeMessageHelper.setText(content,isHtml);
+            mimeMessageHelper.setText(content, isHtml);
             //设置邮件的收件人
             mimeMessageHelper.setTo(toWho);
             //设置非必要的邮件元素，在使用helper进行封装时，这些数据都不能够为空
-            if(ccPeoples != null)
+            if (ccPeoples != null)
                 //设置邮件的抄送人：MimeMessageHelper # Assert.notNull(cc, "Cc address array must not be null");
                 mimeMessageHelper.setCc(ccPeoples);
 
-            if(bccPeoples != null)
+            if (bccPeoples != null)
                 //设置邮件的密送人：MimeMessageHelper # Assert.notNull(bcc, "Bcc address array must not be null");
                 mimeMessageHelper.setBcc(bccPeoples);
             return true;
-        }catch(MessagingException e){
+        } catch (MessagingException e) {
             e.printStackTrace();
-            log.error("邮件基本信息出错->{}",subject);
+            log.error("邮件基本信息出错->{}", subject);
         }
         return false;
     }
 
     @Override
-    public void handleBasicInfo(SimpleMailMessage simpleMailMessage,String subject,String content,String[] toWho,String[] ccPeoples,String[] bccPeoples){
+    public void handleBasicInfo(SimpleMailMessage simpleMailMessage, String subject, String content, String[] toWho, String[] ccPeoples, String[] bccPeoples) {
         //设置发件人
-        simpleMailMessage.setFrom(nickname+'<'+userName+'>');
+        simpleMailMessage.setFrom(nickname + '<' + userName + '>');
         //设置邮件的主题
         simpleMailMessage.setSubject(subject);
         //设置邮件的内容
@@ -149,25 +151,25 @@ public class MailerServiceImpl implements MailService {
     }
 
     @Override
-    public void handleBasicInfo(MimeMessageHelper mimeMessageHelper,String subject,String content,String[] toWho){
+    public void handleBasicInfo(MimeMessageHelper mimeMessageHelper, String subject, String content, String[] toWho) {
         try {
             //设置发件人
-            mimeMessageHelper.setFrom(nickname+'<'+userName+'>');
+            mimeMessageHelper.setFrom(nickname + '<' + userName + '>');
             //设置邮件的主题
             mimeMessageHelper.setSubject(subject);
             //设置邮件的内容
-            mimeMessageHelper.setText(content,true);
+            mimeMessageHelper.setText(content, true);
             //设置邮件的收件人
             mimeMessageHelper.setTo(toWho);
         } catch (MessagingException e) {
-            log.error("html邮件基本信息出错->{}",subject);
+            log.error("html邮件基本信息出错->{}", subject);
         }
     }
 
     @Override
-    public void handleAttachment(MimeMessageHelper mimeMessageHelper,String subject,String[] attachmentFilePaths){
+    public void handleAttachment(MimeMessageHelper mimeMessageHelper, String subject, String[] attachmentFilePaths) {
         //判断是否需要处理邮件的附件
-        if(attachmentFilePaths != null&&attachmentFilePaths.length > 0) {
+        if (attachmentFilePaths != null && attachmentFilePaths.length > 0) {
             FileSystemResource resource;
             String fileName;
             //循环处理邮件的附件
