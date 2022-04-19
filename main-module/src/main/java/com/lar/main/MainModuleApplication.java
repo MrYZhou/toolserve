54737378eaa0da41068517b9187c2968738553c7
larry <1762861715@qq.com>
2022-04-20 00:18:51 +0800

    security初步

--- main-module/src/main/java/com/lar/main/MainModuleApplication.java
+++ main-module/src/main/java/com/lar/main/MainModuleApplication.java
@@ -1,32 +1,37 @@
 package com.lar.main;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+/** bean扫描 */
 @ComponentScans({
-        @ComponentScan("com.lar"),
-        @ComponentScan("common"),
-        @ComponentScan("middle"),
+  @ComponentScan("com.lar"),
+  @ComponentScan("common"),
+  @ComponentScan("middle"),
 })
+// 允许异步
 @EnableAsync
+// jpa扫描
+@EnableJpaRepositories(basePackages = {"com.lar.main", "com.lar.security"})
+@EntityScan(basePackages = {"com.lar"})
 public class MainModuleApplication {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-
+    ConfigurableApplicationContext run =
         new SpringApplicationBuilder()
-                .sources(MainModuleApplication.class)
-                .bannerMode(Banner.Mode.OFF)
-                .run(args);
-
-
-//        SpringApplication.run(MainModuleApplication.class, args);
-    }
-
+            .sources(MainModuleApplication.class)
+            .bannerMode(Banner.Mode.OFF)
+            .run(args);
+    System.out.println(run);
+    //        SpringApplication.run(MainModuleApplication.class, args);
+  }
 }
---
+++ security-module/src/main/java/com/lar/security/main/user/UserController.java
@@ -0,0 +1,20 @@
+package com.lar.security.main.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+  @Autowired UserService userService;
+
+  @RequestMapping(value = "/list")
+  public List login() {
+    UserEntity userEntity = new UserEntity();
+    userService.login(userEntity);
+    return null;
+  }
+}
