78b503841d223abdb64d29fbc10c769cb4648aeb
larry <1762861715@qq.com>
2022-04-23 23:57:01 +0800

    权限初步

--- main-module/src/main/java/com/lar/main/plan/PlanController.java
+++ main-module/src/main/java/com/lar/main/plan/PlanController.java
@@ -2,6 +2,7 @@ package com.lar.main.plan;
 
 import common.base.AppResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class PlanController {
   @Autowired private PlanService planService;
 
   @RequestMapping(value = "/list")
+  @PreAuthorize("hasAnyAuthority('test')")
   public AppResult getPlanList() {
 
     return AppResult.success(planService.findAll());
--- security-module/src/main/java/com/lar/security/main/model/LoginUser.java
+++ security-module/src/main/java/com/lar/security/main/model/LoginUser.java
@@ -1,12 +1,16 @@
-package com.lar.security.main.user;
+package com.lar.security.main.model;
 
+import com.lar.security.main.user.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /** 重写默认UserDetails实现 */
 @Data
@@ -15,9 +19,23 @@ import java.util.Collection;
 public class LoginUser implements UserDetails {
   private UserEntity user;
 
+  private List<String> permissions;
+
+  List<SimpleGrantedAuthority> authorities;
+
+  public LoginUser(UserEntity user, List<String> permissions) {
+    this.user = user;
+    this.permissions = permissions;
+  }
+
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    if (authorities != null) {
+      return authorities;
+    }
+    List<SimpleGrantedAuthority> authorities =
+        permissions.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    return authorities;
   }
 
   @Override
