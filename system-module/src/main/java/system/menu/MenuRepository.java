114e8b6b78131d67b827235eb8f1efd9f6510f1a
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-05-01 13:53:57 +0800

    修改泛型

--- system-module/src/main/java/system/menu/MenuRepository.java
+++ system-module/src/main/java/system/menu/MenuRepository.java
@@ -2,9 +2,11 @@ package system.menu;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, String> {
   @Query(
       value =
