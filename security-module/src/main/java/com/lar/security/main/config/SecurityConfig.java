62ffff8c145f1026d4d3dba7e1583544d1b49c76
larry <1762861715@qq.com>
2022-05-01 13:53:57 +0800

    修改泛型

--- security-module/src/main/java/com/lar/security/main/config/SecurityConfig.java
+++ security-module/src/main/java/com/lar/security/main/config/SecurityConfig.java
@@ -50,10 +50,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         // 对于登录接口 允许匿名访问
         .antMatchers("/user/login")
-        .anonymous()
+        .anonymous();
         // 除上面外的所有请求全部需要鉴权认证
-        .anyRequest()
-        .authenticated();
+        // .anyRequest()
+        // .authenticated();
 
     // 把token校验过滤器添加到过滤器链中,并且在验证账号密码之前
     http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
