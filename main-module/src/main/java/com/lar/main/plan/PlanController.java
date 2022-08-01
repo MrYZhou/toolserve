ae2411f7ba66c217ca51778cca0361ada1310b80
larry <1762861715@qq.com>
2022-08-01 21:15:10 +0800

    提交

--- main-module/src/main/java/com/lar/main/plan/PlanController.java
+++ main-module/src/main/java/com/lar/main/plan/PlanController.java
@@ -1,8 +1,12 @@
 package com.lar.main.plan;
 
+import com.lar.main.plan.model.PlanQuery;
 import common.base.AppResult;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,11 @@ public class PlanController {
     @PreAuthorize("hasAnyAuthority('test')")
     public AppResult getPlanList() {
         return AppResult.success(planService.findAll());
+    }
+
+    @GetMapping("/page")
+    public AppResult page(@RequestBody PlanQuery query) {
+        Page<PlanEntity> data = planService.page(query);
+        return AppResult.success(data);
     }
 }
--- main-module/src/main/java/com/lar/main/plan/PlanService.java
+++ main-module/src/main/java/com/lar/main/plan/PlanService.java
@@ -1,10 +1,13 @@
 package com.lar.main.plan;
 
-import org.springframework.stereotype.Component;
+import com.lar.main.plan.model.PlanQuery;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface PlanService {
 
     List findAll();
+
+    Page<PlanEntity> page(PlanQuery query);
 }
