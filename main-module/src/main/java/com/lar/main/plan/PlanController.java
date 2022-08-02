a21535aa78ebef41df76277bafa5f22a967baaf2
larry <*\(^_^)/*>
2022-08-02 10:45:12 +0800

    使用雪花id

--- main-module/src/main/java/com/lar/main/plan/PlanController.java
+++ main-module/src/main/java/com/lar/main/plan/PlanController.java
@@ -3,6 +3,7 @@ package com.lar.main.plan;
 import com.lar.main.plan.model.PlanQuery;
 import common.base.AppResult;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class PlanController {
 
+    @Autowired
     private PlanService planService;
+    @Autowired
+    private PlanRepositoty planReposity;
 
     @RequestMapping("/list")
     @PreAuthorize("hasAnyAuthority('test')")
     public AppResult getPlanList() {
         return AppResult.success(planService.findAll());
+    }
+
+    @GetMapping("/add")
+    public AppResult add() {
+        PlanEntity planEntity = new PlanEntity();
+        planEntity.setName("一");
+        return AppResult.success(planReposity.save(planEntity));
     }
 
     @GetMapping("/page")
