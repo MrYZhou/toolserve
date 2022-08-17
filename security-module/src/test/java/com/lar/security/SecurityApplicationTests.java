192700ee1d28464553b7c33bb5840cd6bfe9f9a4
lingen <*\(^_^)/*>
2022-08-17 10:49:57 +0800

    结构调整

---
+++ security-module/src/test/java/com/lar/security/SecurityApplicationTests.java
@@ -0,0 +1,10 @@
+package com.lar.security;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SecurityApplicationTests {
+//    @Test
+//    void testencode() {
+//    }
+}
