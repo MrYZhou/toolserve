5c50025ecab81c37efbad9ce0acb24e46074c04d
larry <*\(^_^)/*>
2022-08-17 18:01:54 +0800

    时间

--- main-module/src/main/java/com/lar/main/plan/model/PlanQuery.java
+++ main-module/src/main/java/com/lar/main/plan/model/PlanQuery.java
@@ -2,9 +2,13 @@ package com.lar.main.plan.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class PlanQuery {
-    
+
+    //    private Date startTime;
+    LocalDateTime startTime = LocalDateTime.of(2000, 1, 1, 1, 1);
     private Integer page;
     private Integer size;
 }
