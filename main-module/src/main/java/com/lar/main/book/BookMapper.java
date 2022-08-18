c4246191bc6d939a888bebebea23e359233dbf3e
lingen <*\(^_^)/*>
2022-08-18 17:21:26 +0800

    mybatisplus crud例子

---
+++ main-module/src/main/java/com/lar/main/book/BookMapper.java
@@ -0,0 +1,9 @@
+package com.lar.main.book;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface BookMapper extends BaseMapper<BookEntity> {
+}
