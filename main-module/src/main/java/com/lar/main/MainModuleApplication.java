af47363f2414bcc079ace503937ad6afdf9f79c3
lingen <*\(^_^)/*>
2022-08-18 17:20:06 +0800

    mybatisplus配置

--- main-module/src/main/java/com/lar/main/MainModuleApplication.java
+++ main-module/src/main/java/com/lar/main/MainModuleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,10 +12,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 /** bean扫描 */
 @ComponentScans({
-  @ComponentScan("com.lar"),
-  @ComponentScan("common"),
-  @ComponentScan("middle"),
-  @ComponentScan("system")
+        @ComponentScan("com.lar"),
+        @ComponentScan("common"),
+        @ComponentScan("middle"),
+        @ComponentScan("system")
 })
 // 允许异步
 @EnableAsync
@@ -26,14 +25,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EntityScan(basePackages = {"com.lar", "system"})
 public class MainModuleApplication {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    ConfigurableApplicationContext run =
+
         new SpringApplicationBuilder()
-            .sources(MainModuleApplication.class)
-            .bannerMode(Banner.Mode.OFF)
-            .run(args);
-    System.out.println(run);
-    //        SpringApplication.run(MainModuleApplication.class, args);
-  }
+                .sources(MainModuleApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
 }
