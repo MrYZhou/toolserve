97e9f99fedef375b3f4767d887214af3439c8065
lingen <*\(^_^)/*>
2022-08-18 15:25:31 +0800

    mybatisplus配置类

---
+++ common-module/src/main/java/common/config/database/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package common.config.database;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.lar.main")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+
+}
