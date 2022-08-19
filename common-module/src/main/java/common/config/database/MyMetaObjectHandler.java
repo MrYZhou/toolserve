d0fac4661eabd149ae6c4b97bc05935f9345f5fe
lingen <*\(^_^)/*>
2022-08-19 16:29:36 +0800

    字段自动填充

--- common-module/src/main/java/common/config/database/MyMetaObjectHandler.java
+++ common-module/src/main/java/common/config/database/MyMetaObjectHandler.java
@@ -4,20 +4,22 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
 
+
     @Override
     public void insertFill(MetaObject metaObject) {
 
-        this.setFieldValByName("createTime", new Date(), metaObject);
-        this.setFieldValByName("modifyTime", new Date(), metaObject);
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, "modifyTime", LocalDateTime::now, LocalDateTime.class);
+        this.setFieldValByName("tag", "lar", metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("modifyTime", new Date(), metaObject);
+        this.strictInsertFill(metaObject, "modifyTime", LocalDateTime::now, LocalDateTime.class);
     }
 }
