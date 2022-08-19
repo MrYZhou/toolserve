5a38b162e0cb934dcec877b50cf7f064c2df4886
lingen <*\(^_^)/*>
2022-08-19 16:40:31 +0800

    动态表名

--- common-module/src/main/java/common/config/database/MybatisPlusConfig.java
+++ common-module/src/main/java/common/config/database/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package common.config.database;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,16 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+
+        DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
+        dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
+            // 获取当前的动态表名
+            String tag = "";
+            return tableName + tag;
+        });
+        interceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
+
         return interceptor;
     }
 
