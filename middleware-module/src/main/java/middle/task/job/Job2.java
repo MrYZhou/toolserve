699f28036ba4aeb0453a232119a3ddf77b14056b
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-09-05 00:47:42 +0800

    quartz

---
+++ middleware-module/src/main/java/middle/task/job/Job2.java
@@ -0,0 +1,16 @@
+package middle.task.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+@DisallowConcurrentExecution
+@PersistJobDataAfterExecution
+public class Job2 extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        System.out.println(11);
+    }
+}
