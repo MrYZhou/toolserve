5b46a788acf89fdf11e1932f4c1ab380660af387
lingen <*\(^_^)/*>
2022-09-05 14:22:44 +0800

    项目依赖

--- main-module/src/test/java/com/lar/main/SecurityApplicationTests.java
+++ main-module/src/test/java/com/lar/main/SecurityApplicationTests.java
@@ -1,10 +1,11 @@
-package com.lar.security;
+package com.lar.main;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SecurityApplicationTests {
-//    @Test
-//    void testencode() {
-//    }
+    @Test
+    void testencode() {
+    }
 }
--- middleware-module/src/main/java/middle/task/config/ScheduleConfig.java
+++ middleware-module/src/main/java/middle/task/config/ScheduleConfig.java
@@ -1,19 +1,9 @@
 package middle.task.config;
 
-import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class ScheduleConfig {
@@ -21,49 +11,71 @@ public class ScheduleConfig {
     @Autowired
     private DataSource dataSource;
 
-    @Bean
-    public Scheduler scheduler() throws IOException {
-        schedulerProperties();
-        return  schedulerFactory().getScheduler();
-    }
-    @Bean
-    public SchedulerFactoryBean schedulerFactory() throws IOException {
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-        factory.setDataSource(dataSource);
+    // 除了在springboot生命周期配置, 在这里配置的触发器bean会自动被触发，但是这种好像非常耗费内存。推测是quartz频繁访问同一个对象、
+//    @Bean
+//    public JobDetail testJobDetail() {
+//        JobDetail JobDetail = JobBuilder.newJob(Job3.class)
+//                .storeDurably() //必须调用该方法，添加任务
+//                .build();
+//
+//        return JobDetail;
+//    }
+//
+//    @Bean
+//    public Trigger testTrigger() {
+//        Trigger trigger = TriggerBuilder.newTrigger()
+//                .forJob(testJobDetail())
+//                .withSchedule(CronScheduleBuilder.cronSchedule("0/3 * * * * ?")) //对触发器配置任务
+//                .build();
+//        return trigger;
+//    }
 
-
-        factory.setQuartzProperties(schedulerProperties());
-
-        //调度标识名 集群中每一个实例都必须使用相同的名称
-        factory.setSchedulerName("scheduler");
-
-        factory.setApplicationContextSchedulerContextKey("applicationContextKey");
-        // 可选，QuartzScheduler
-        // 启动时更新己存在的Job，这样就不用每次修改targetObject后删除qrtz_job_details表对应记录了
-        factory.setOverwriteExistingJobs(true);
-        // 设置自动启动，默认为true
-        factory.setAutoStartup(true);
-        factory.setTaskExecutor(getExecutor() );
-        // 延时启动
-        factory.setStartupDelay(0);
-        return factory;
-    }
-    @Bean
-    public Properties schedulerProperties() throws IOException {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-//        propertiesFactoryBean.setLocation(new ClassPathResource("/application-dev.yml"));
-        propertiesFactoryBean.afterPropertiesSet();
-        return propertiesFactoryBean.getObject();
-    }
-
-    @Bean
-    public Executor getExecutor() throws IOException {
-        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
-        int core = Runtime.getRuntime().availableProcessors();
-        threadPoolExecutor.setCorePoolSize(core);
-        threadPoolExecutor.setMaxPoolSize(core);
-        threadPoolExecutor.setQueueCapacity(core);
-        return threadPoolExecutor;
-    }
+    // 下面的配置需要生成quartz的sql表才可以使用
+//    @Bean
+//    public Scheduler scheduler() throws IOException {
+//        schedulerProperties();
+//        return schedulerFactory().getScheduler();
+//    }
+//
+//    @Bean
+//    public SchedulerFactoryBean schedulerFactory() throws IOException {
+//        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+//        factory.setDataSource(dataSource);
+//
+//
+//        factory.setQuartzProperties(schedulerProperties());
+//
+//        //调度标识名 集群中每一个实例都必须使用相同的名称
+//        factory.setSchedulerName("scheduler");
+//
+//        factory.setApplicationContextSchedulerContextKey("applicationContextKey");
+//        // 可选，QuartzScheduler
+//        // 启动时更新己存在的Job，这样就不用每次修改targetObject后删除qrtz_job_details表对应记录了
+//        factory.setOverwriteExistingJobs(true);
+//        // 设置自动启动，默认为true
+//        factory.setAutoStartup(true);
+//        factory.setTaskExecutor(getExecutor());
+//        // 延时启动
+//        factory.setStartupDelay(0);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public Properties schedulerProperties() throws IOException {
+//        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+////        propertiesFactoryBean.setLocation(new ClassPathResource("/application-dev.yml"));
+//        propertiesFactoryBean.afterPropertiesSet();
+//        return propertiesFactoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public Executor getExecutor() throws IOException {
+//        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+//        int core = Runtime.getRuntime().availableProcessors();
+//        threadPoolExecutor.setCorePoolSize(core);
+//        threadPoolExecutor.setMaxPoolSize(core);
+//        threadPoolExecutor.setQueueCapacity(core);
+//        return threadPoolExecutor;
+//    }
 
 }
