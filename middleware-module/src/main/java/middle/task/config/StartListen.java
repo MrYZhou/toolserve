7e6a0c7506a432b8722d2f28e812ac836e99d76b
larry <*\(^_^)/*>
2022-09-07 16:53:15 +0800

    定时SchedulerFactoryBean注入

--- middleware-module/src/main/java/middle/task/config/StartListen.java
+++ middleware-module/src/main/java/middle/task/config/StartListen.java
@@ -5,12 +5,19 @@ import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 // 初始化定时需要,先注释不用
 //@Component
 public class StartListen implements ApplicationListener<ContextRefreshedEvent> {
     @Autowired
     Scheduler scheduler;
+    /**
+     * 必须在生命周期ApplicationListener下注入，其他地方注入可能导致
+     * bean还未生成
+     */
+    @Autowired
+    SchedulerFactoryBean schedulerFactory;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -23,7 +30,9 @@ public class StartListen implements ApplicationListener<ContextRefreshedEvent> {
                 .build();
 
         try {
+            Scheduler scheduler = null;
 //         Scheduler   scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler = schedulerFactory.getScheduler();
             scheduler.scheduleJob(job1, trigger1);
             scheduler.start();
         } catch (SchedulerException e) {
