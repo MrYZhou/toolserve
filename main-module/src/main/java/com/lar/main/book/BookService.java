ebdff8a9c0d586ff5176e881cf4b9d5748f85c9f
lingen <*\(^_^)/*>
2022-09-27 17:43:02 +0800

    mybatis的xml查询和文件上传

--- main-module/src/main/java/com/lar/main/book/BookService.java
+++ main-module/src/main/java/com/lar/main/book/BookService.java
@@ -1,6 +1,10 @@
 package com.lar.main.book;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.lar.main.book.model.BookPage;
+
+import java.util.List;
 
 public interface BookService extends IService<BookEntity> {
+    List<BookEntity> getList(BookPage page);
 }
