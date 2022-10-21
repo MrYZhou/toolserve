7b3ebcb48e93d4b6f7d8b2e0eef4b1ab7dddaae5
lingen <*\(^_^)/*>
2022-10-21 17:33:12 +0800

    model提交

--- main-module/src/main/java/com/lar/main/book/BookController.java
+++ main-module/src/main/java/com/lar/main/book/BookController.java
@@ -1,6 +1,7 @@
 package com.lar.main.book;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.http.HttpUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.read.listener.PageReadListener;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -28,6 +29,85 @@ public class BookController {
         BookPage info = bookService.page(page, wrapper);
 //        List<BookEntity> books = bookService.getList(page);
         return AppResult.success(info);
+    }
+
+    @PostMapping("mus")
+    public AppResult<Object> mus(@RequestBody @Validated BookPage page) {
+        String s = HttpUtil.get("http://v.juhe.cn/laohuangli/d?date=2014-09-11&key=3204cdf5a2a0c36778c880a461b6ea5d");
+        return AppResult.success(s);
+    }
+
+    @GetMapping
+    public AppResult<Object> tuanqi() {
+        String data = "{\n" +
+                "    \"reason\": \"查询成功\",\n" +
+                "    \"result\": {\n" +
+                "        \"city\": \"苏州\",\n" +
+                "        \"realtime\": {\n" +
+                "            \"temperature\": \"4\",\n" +
+                "            \"humidity\": \"82\",\n" +
+                "            \"info\": \"阴\",\n" +
+                "            \"wid\": \"02\",\n" +
+                "            \"direct\": \"西北风\",\n" +
+                "            \"power\": \"3级\",\n" +
+                "            \"aqi\": \"80\"\n" +
+                "        },\n" +
+                "        \"future\": [\n" +
+                "            {\n" +
+                "                \"date\": \"2019-02-22\",\n" +
+                "                \"temperature\": \"1/7℃\",\n" +
+                "                \"weather\": \"小雨转多云\",\n" +
+                "                \"wid\": {\n" +
+                "                    \"day\": \"07\",\n" +
+                "                    \"night\": \"01\"\n" +
+                "                },\n" +
+                "                \"direct\": \"北风转西北风\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"date\": \"2019-02-23\",\n" +
+                "                \"temperature\": \"2/11℃\",\n" +
+                "                \"weather\": \"多云转阴\",\n" +
+                "                \"wid\": {\n" +
+                "                    \"day\": \"01\",\n" +
+                "                    \"night\": \"02\"\n" +
+                "                },\n" +
+                "                \"direct\": \"北风转东北风\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"date\": \"2019-02-24\",\n" +
+                "                \"temperature\": \"6/12℃\",\n" +
+                "                \"weather\": \"多云\",\n" +
+                "                \"wid\": {\n" +
+                "                    \"day\": \"01\",\n" +
+                "                    \"night\": \"01\"\n" +
+                "                },\n" +
+                "                \"direct\": \"东北风转北风\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"date\": \"2019-02-25\",\n" +
+                "                \"temperature\": \"5/12℃\",\n" +
+                "                \"weather\": \"小雨转多云\",\n" +
+                "                \"wid\": {\n" +
+                "                    \"day\": \"07\",\n" +
+                "                    \"night\": \"01\"\n" +
+                "                },\n" +
+                "                \"direct\": \"东北风\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"date\": \"2019-02-26\",\n" +
+                "                \"temperature\": \"5/11℃\",\n" +
+                "                \"weather\": \"多云转小雨\",\n" +
+                "                \"wid\": {\n" +
+                "                    \"day\": \"01\",\n" +
+                "                    \"night\": \"07\"\n" +
+                "                },\n" +
+                "                \"direct\": \"东北风\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    \"error_code\": 0\n" +
+                "}";
+        return new AppResult<>();
     }
 
     @GetMapping("/{id}")
