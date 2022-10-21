d332caed19945e0efe0ece8945e89c00538bcf57
larry <*\(^_^)/*>
2022-10-21 17:33:12 +0800

    model提交

---
+++ main-module/src/main/java/com/lar/main/xlj/model/TianRootBean.java
@@ -0,0 +1,40 @@
+package com.lar.main.xlj.model;
+
+
+/**
+ * Auto-generated: 2022-10-21 17:17:49
+ *
+ * @author json.cn (i@json.cn)
+ * @website http://www.json.cn/java2pojo/
+ */
+public class TianRootBean {
+
+    private String reason;
+    private Realtime result;
+    private int error_code;
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setResult(Realtime result) {
+        this.result = result;
+    }
+
+    public Realtime getResult() {
+        return result;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+}
