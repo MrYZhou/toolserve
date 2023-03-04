97512fcd2bd91ed049d352d9a93b3f85a3814d6a
larry <1762861715@qq.com>
2023-03-05 01:02:27 +0800

    satoken

--- security-module/src/main/java/com/lar/security/user/UserController.java
+++ security-module/src/main/java/com/lar/security/user/UserController.java
@@ -1,5 +1,7 @@
 package com.lar.security.user;
 
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
 import com.lar.security.model.UserView;
 import com.lar.common.base.AppResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +21,33 @@ public class UserController {
     return null;
   }
 
-  @PostMapping(value = "/login")
-  public AppResult login(@RequestBody UserView userView) {
-
-    return userService.login(userView);
+  // 测试登录  ---- http://localhost:8081/acc/doLogin?name=zhang&pwd=123456
+  @RequestMapping("doLogin")
+  public SaResult doLogin(String name, String pwd) {
+    // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+    if("zhang".equals(name) && "123456".equals(pwd)) {
+      StpUtil.login(10001);
+      return SaResult.ok("登录成功");
+    }
+    return SaResult.error("登录失败");
   }
 
-  @GetMapping(value = "/logout")
-  public AppResult logout() {
+  // 查询登录状态  ---- http://localhost:8081/acc/isLogin
+  @RequestMapping("isLogin")
+  public SaResult isLogin() {
+    return SaResult.ok("是否登录：" + StpUtil.isLogin());
+  }
 
-    return userService.logout();
+  // 查询 Token 信息  ---- http://localhost:8081/acc/tokenInfo
+  @RequestMapping("tokenInfo")
+  public SaResult tokenInfo() {
+    return SaResult.data(StpUtil.getTokenInfo());
+  }
+
+  // 测试注销  ---- http://localhost:8081/acc/logout
+  @RequestMapping("logout")
+  public SaResult logout() {
+    StpUtil.logout();
+    return SaResult.ok();
   }
 }
