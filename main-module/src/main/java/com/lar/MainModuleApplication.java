c9d7ab3a0798ea1a4bb238a9d5934dea039be295
lingen <1762861715@qq.com>
2023-03-08 16:17:57 +0800

    bannner修改

--- main-module/src/main/java/com/lar/MainModuleApplication.java
+++ main-module/src/main/java/com/lar/MainModuleApplication.java
@@ -1,26 +1,23 @@
 package com.lar;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
 /**
  * bean扫描 ,默认是SpringBootApplication注解的所在包为根路径
- *
- * @SpringBootApplication 相当于@ComponentScans({
- * @ComponentScan("com.lar") })
+ * &#064;SpringBootApplication  相当于@ComponentScans({  &#064;ComponentScan("com.lar")  })
  */
 @SpringBootApplication
 // 允许异步
 @EnableAsync
 public class MainModuleApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = new SpringApplicationBuilder()
-                .sources(MainModuleApplication.class)
-//                .bannerMode(Banner.Mode.OFF)
-                .run(args);
+        SpringApplication app = new SpringApplicationBuilder().sources(MainModuleApplication.class).bannerMode(Banner.Mode.OFF).build(args);
+        app.run();
 //        System.out.println("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
     }
 }
