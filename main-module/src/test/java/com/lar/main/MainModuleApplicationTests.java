3ec2a73279cc66189f17e800dc6fa9e698e415d1
lingen <1762861715@qq.com>
2023-03-08 15:56:28 +0800

    移除freemarker

--- main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
+++ main-module/src/test/java/com/lar/main/MainModuleApplicationTests.java
@@ -1,19 +1,12 @@
 package com.lar.main;
 
-import com.lar.example.FreeMarkerUtil;
 import com.lar.common.util.EmailTask;
 import com.lar.common.util.RedisUtil;
-import freemarker.template.TemplateException;
-import com.lar.middle.task.email.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,33 +19,11 @@ class MainModuleApplicationTests {
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
-    private MailService mailService;
-
-    @Autowired
-    FreeMarkerUtil freeMarkerUtil;
 
     @Test
     void redisTest() {
         redisUtil.set("testutil", "success");
     }
 
-
-  
-    @Test
-    public void gen() throws IOException, TemplateException {
-        //生成freemarker的模板对象，调用方法获取模板
-//        Template template = configuration.getTemplate("1.ftl");
-
-        freeMarkerUtil.getTemplate("1.ftl");
-
-        //获取数据的过程,这里自定义封装成了方法
-        Map params = new HashMap();
-        params.put("package", "books");
-        params.put("json", "'{\"name\":\"larry\"}'");
-        freeMarkerUtil.render(params, "D:/book.java");
-        //合成
-//        template.process(params, new FileWriter("D:/book.java")); //第一个参数：数据模型，第二个参数：输出流
-    }
 
 }
