9287123ff639939a9f58a9d7b5f05016e0ca13a0
larry <1762861715@qq.com>
2023-03-09 18:02:22 +0800

    easy trans翻译

--- main-module/src/main/java/com/lar/book/model/BookInfo.java
+++ main-module/src/main/java/com/lar/book/model/BookInfo.java
@@ -1,16 +1,17 @@
 package com.lar.book.model;
 
-import com.fhs.core.trans.anno.Trans;
-import com.fhs.core.trans.constant.TransType;
 import com.fhs.core.trans.vo.TransPojo;
 import lombok.Data;
 
 @Data
 public class BookInfo implements TransPojo {
+
     String id;
     String name;
+
     String tag;
     // 字典翻译 ref为非必填
-    @Trans(type = TransType.DICTIONARY, key = "book")
+//    @Trans(type = TransType.DICTIONARY, key = "book")
+    @DictValue(ref = "book")
     String type;
 }
