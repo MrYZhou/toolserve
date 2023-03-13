97c59ec22331fddce76c8d0a8b69f91f5d814f4e
lingen <1762861715@qq.com>
2023-03-13 17:36:17 +0800

    字典翻译

--- main-module/src/main/java/com/lar/book/model/BookInfo.java
+++ main-module/src/main/java/com/lar/book/model/BookInfo.java
@@ -1,8 +1,7 @@
 package com.lar.book.model;
 
-import com.fhs.core.trans.anno.Trans;
-import com.fhs.core.trans.constant.TransType;
-import com.lar.trans.DictValue;
+
+import com.larry.trans.DictValue;
 import lombok.Data;
 
 @Data
@@ -16,6 +15,6 @@ public class BookInfo {
     String type;
 
     //SIMPLE 翻译，用于关联其他的表进行翻译    schoolName 为 School 的一个字段
-    @Trans(type = TransType.SIMPLE, target = Place.class, fields = "name")
+//    @Trans(type = TransType.SIMPLE, target = Place.class, fields = "name")
     private String placeId;
 }
