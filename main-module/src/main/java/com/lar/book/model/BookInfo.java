1956d118f2d963b8ba8f9647594066547ffdf77b
larry <1762861715@qq.com>
2023-03-23 21:53:45 +0800

    字典翻译

--- main-module/src/main/java/com/lar/book/model/BookInfo.java
+++ main-module/src/main/java/com/lar/book/model/BookInfo.java
@@ -1,6 +1,5 @@
 package com.lar.book.model;
 
-
 import com.larry.trans.DictValue;
 import lombok.Data;
 
@@ -14,7 +13,4 @@ public class BookInfo {
     @DictValue(ref = "book")
     String type;
 
-    //SIMPLE 翻译，用于关联其他的表进行翻译    schoolName 为 School 的一个字段
-//    @Trans(type = TransType.SIMPLE, target = Place.class, fields = "name")
-    private String placeId;
 }
