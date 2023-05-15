0256c5ad1efa186e2dd8f13d3e86a7c41fb2cfab
larry <>
2023-05-15 17:28:16 +0800

    重构目录结构,尤其是commone模块目录错误

--- common-module/src/main/java/com/lar/CommonApplication.java
+++ common-module/src/main/java/com/lar/CommonApplication.java
@@ -1,5 +1,7 @@
+package com.lar;
+
 public class CommonApplication {
     public static void main(String[] args) {
-        System.out.println("common module");
+        System.out.println("common module" );
     }
 }
