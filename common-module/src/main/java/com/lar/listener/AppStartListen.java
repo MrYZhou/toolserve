3c3a0661f8a575f42335f8c05e9368aaaac1213d
lg <>
2023-05-16 11:31:00 +0800

    注释修改

--- common-module/src/main/java/com/lar/listener/AppStartListen.java
+++ common-module/src/main/java/com/lar/listener/AppStartListen.java
@@ -6,24 +6,24 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.boot.system.JavaVersion;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-//@Component
-// 通过classloader
+@Component
+// 通过classloader,存在类
 @ConditionalOnClass
-// 通过spring的beanFactory
+// 通过spring的beanFactory中存在bean
 //@ConditionalOnBean
 // 在jdk版本生效
 @ConditionalOnJava(range = ConditionalOnJava.Range.EQUAL_OR_NEWER, value = JavaVersion.SEVENTEEN)
 // 通过配置文件
 //@ConditionalOnProperty
-// 判断环境
+// 判断环境是web
 @ConditionalOnWebApplication
 public class AppStartListen implements ApplicationListener<ContextRefreshedEvent> {
 
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        // 当应用器启动的时候
-
+        // 当应用启动的时候
     }
 }
