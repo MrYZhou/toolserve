233280f9fa81063c632c5a874a0267149f101bd9
larry <lingeng@yinmaisoft.com>
2023-06-20 11:29:33 +0800

    结构调整

--- security-module/src/main/java/com/lar/security/user/server/UserService.java
+++ security-module/src/main/java/com/lar/security/user/server/UserService.java
@@ -1,6 +1,6 @@
-package com.lar.security.user.domain.server;
+package com.lar.security.user.server;
 
-import com.lar.security.user.domain.repository.jpa.UserEntity;
+import com.lar.security.user.repository.UserEntity;
 
 //对复杂应用可以使用，如调用超过4个服务或仓库领域,否则这一层可以空置
 public interface UserService {
