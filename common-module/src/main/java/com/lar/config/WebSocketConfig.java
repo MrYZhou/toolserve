b3da8cd2c816b75abcad3b8fc5296ea6dcf613b5
larry <1762861715@qq.com>
2023-06-24 22:56:34 +0800

    增加测试

--- common-module/src/main/java/com/lar/config/WebSocketConfig.java
+++ common-module/src/main/java/com/lar/config/WebSocketConfig.java
@@ -1,18 +1,14 @@
 package com.lar.config;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
 /**
  * 端口暴露
  */
-@Configuration
-public class WebSocketConfig {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
-
-}
+//@Configuration
+//public class WebSocketConfig {
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
+//
+//}
