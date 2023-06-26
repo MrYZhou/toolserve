6700c8cbfc4f08b141435bac4ac7e8c8e1241e72
lingen <lingeng@yinmaisoft.com>
2023-06-26 16:27:40 +0800

    dbcontext使用HikariDataSource

--- common-module/src/main/java/com/lar/config/database/WoodConfig.java
+++ common-module/src/main/java/com/lar/config/database/WoodConfig.java
@@ -1,14 +1,16 @@
 package com.lar.config.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Data;
 import org.noear.wood.DbContext;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(
-        prefix = "spring.datasource"
-)
+@ConfigurationProperties(prefix = "spring.datasource")
+@Data
 public class WoodConfig {
     private String url;
 
@@ -17,6 +19,12 @@ public class WoodConfig {
 
     @Bean
     public DbContext getDbContext() {
-        return new DbContext("", url, username, password);
+        // 创建HikariCP连接池
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return new DbContext("", dataSource);
     }
 }
