70649533f2d72df61994b91459abd2b8afba8381
larry <lingeng@yinmaisoft.com>
2023-06-26 17:21:16 +0800

    数据库文件更新

--- main-module/src/main/resources/static/study.sql
+++ main-module/src/main/resources/static/study.sql
@@ -11,74 +11,54 @@
  Target Server Version : 50732
  File Encoding         : 65001
 
- Date: 16/05/2023 18:30:11
+ Date: 26/06/2023 17:19:48
 */
 
 SET NAMES utf8mb4;
-SET
-FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for book
 -- ----------------------------
 DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book`
-(
-  `id`         varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `tag`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `book`  (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `createTime` datetime NULL DEFAULT NULL,
   `modifyTime` datetime NULL DEFAULT NULL,
-  `type`       int(2) NULL DEFAULT NULL,
-  `dictId`     varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `type` int(2) NULL DEFAULT NULL,
+  `dictId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book`
-VALUES ('1560526548464287746', 'String', '1233', '2022-08-19 15:18:17', '2022-08-19 15:18:17', 1, '143534634634');
-INSERT INTO `book`
-VALUES ('1560526668400410626', 'String', '1233', '2022-08-19 15:18:46', '2022-08-19 15:18:46', 0, NULL);
-INSERT INTO `book`
-VALUES ('1560527036161179650', 'String', '0vwb5v61b', '2022-08-19 15:20:13', '2022-08-19 15:20:13', NULL,
-        '143534634634');
-INSERT INTO `book`
-VALUES ('1560527441549049858', 'String', 'qba1pt8pb', '2022-08-19 15:21:50', '2022-08-19 15:21:50', NULL,
-        '1435346346342');
-INSERT INTO `book`
-VALUES ('1560545151322406913', 'String', 'lar', '2022-08-19 16:32:12', '2022-08-19 16:32:12', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560545304250925057', 'String', 'lar', '2022-08-19 16:32:49', '2022-08-19 16:32:49', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560545365261271042', 'String', 'lar', '2022-08-19 16:33:03', '2022-08-19 16:33:03', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560545395510591490', 'String', 'lar', '2022-08-19 16:33:10', '2022-08-19 16:33:10', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560545484677300226', 'String', 'lar', '2022-08-19 16:33:32', '2022-08-19 16:33:32', 1, NULL);
-INSERT INTO `book`
-VALUES ('1560545601429950465', 'String', 'lar', '2022-08-19 16:34:00', '2022-08-19 16:34:00', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560546014908633090', 'String', 'lar', '2022-08-19 16:35:38', '2022-08-19 16:35:38', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560548859133276161', 'String', 'lar', '2022-08-19 16:46:56', '2022-08-19 16:46:56', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560548937617096706', 'String', 'lar', '2022-08-19 16:47:15', '2022-08-19 16:47:15', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560549100993626113', 'String', 'lar', '2022-08-19 16:47:54', '2022-08-19 16:47:54', NULL, NULL);
-INSERT INTO `book`
-VALUES ('1560549627299086337', 'String', 'lar', '2022-08-19 16:49:59', '2022-08-19 16:49:59', NULL, NULL);
+INSERT INTO `book` VALUES ('1560526548464287746', 'String', '1233', '2022-08-19 15:18:17', '2022-08-19 15:18:17', 1, '143534634634');
+INSERT INTO `book` VALUES ('1560526668400410626', 'String', '1233', '2022-08-19 15:18:46', '2022-08-19 15:18:46', 0, NULL);
+INSERT INTO `book` VALUES ('1560527036161179650', 'String', '0vwb5v61b', '2022-08-19 15:20:13', '2022-08-19 15:20:13', NULL, '143534634634');
+INSERT INTO `book` VALUES ('1560527441549049858', 'String', 'qba1pt8pb', '2022-08-19 15:21:50', '2022-08-19 15:21:50', NULL, '1435346346342');
+INSERT INTO `book` VALUES ('1560545151322406913', 'String', 'lar', '2022-08-19 16:32:12', '2022-08-19 16:32:12', NULL, NULL);
+INSERT INTO `book` VALUES ('1560545304250925057', 'String', 'lar', '2022-08-19 16:32:49', '2022-08-19 16:32:49', NULL, NULL);
+INSERT INTO `book` VALUES ('1560545365261271042', 'String', 'lar', '2022-08-19 16:33:03', '2022-08-19 16:33:03', NULL, NULL);
+INSERT INTO `book` VALUES ('1560545395510591490', 'String', 'lar', '2022-08-19 16:33:10', '2022-08-19 16:33:10', NULL, NULL);
+INSERT INTO `book` VALUES ('1560545484677300226', 'String', 'lar', '2022-08-19 16:33:32', '2022-08-19 16:33:32', 1, NULL);
+INSERT INTO `book` VALUES ('1560545601429950465', 'String', 'lar', '2022-08-19 16:34:00', '2022-08-19 16:34:00', NULL, NULL);
+INSERT INTO `book` VALUES ('1560546014908633090', 'String', 'lar', '2022-08-19 16:35:38', '2022-08-19 16:35:38', NULL, NULL);
+INSERT INTO `book` VALUES ('1560548859133276161', 'String', 'lar', '2022-08-19 16:46:56', '2022-08-19 16:46:56', NULL, NULL);
+INSERT INTO `book` VALUES ('1560548937617096706', 'String', 'lar', '2022-08-19 16:47:15', '2022-08-19 16:47:15', NULL, NULL);
+INSERT INTO `book` VALUES ('1560549100993626113', 'String', 'lar', '2022-08-19 16:47:54', '2022-08-19 16:47:54', NULL, NULL);
+INSERT INTO `book` VALUES ('1560549627299086337', 'String', 'lar', '2022-08-19 16:49:59', '2022-08-19 16:49:59', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for book12
 -- ----------------------------
 DROP TABLE IF EXISTS `book12`;
-CREATE TABLE `book12`
-(
-  `id`         varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `tag`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `book12`  (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `createTime` datetime NULL DEFAULT NULL,
   `modifyTime` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -87,19 +67,17 @@ CREATE TABLE `book12`
 -- ----------------------------
 -- Records of book12
 -- ----------------------------
-INSERT INTO `book12`
-VALUES ('1560546176519352321', 'String', 'lar', '2022-08-19 16:36:17', '2022-08-19 16:36:17');
+INSERT INTO `book12` VALUES ('1560546176519352321', 'String', 'lar', '2022-08-19 16:36:17', '2022-08-19 16:36:17');
 
 -- ----------------------------
 -- Table structure for config
 -- ----------------------------
 DROP TABLE IF EXISTS `config`;
-CREATE TABLE `config`
-(
-  `id`         varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `cacheKey`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `pk`         varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `config`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cacheKey` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `pk` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `columnList` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -112,32 +90,29 @@ CREATE TABLE `config`
 -- Table structure for database
 -- ----------------------------
 DROP TABLE IF EXISTS `database`;
-CREATE TABLE `database`
-(
-  `id`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `host`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `port`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `user`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `database`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `host` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `port` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `name`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `driver`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `echo`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `driver` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `echo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of database
 -- ----------------------------
-INSERT INTO `database`
-VALUES ('SxrSY8PvHsumM-eai-o9b', '127.0.0.1', '3306', 'root', 'root', 'study', 'mysql+pymysql', 'True');
+INSERT INTO `database` VALUES ('SxrSY8PvHsumM-eai-o9b', '127.0.0.1', '3306', 'root', 'root', 'study', 'mysql+pymysql', 'True');
 
 -- ----------------------------
 -- Table structure for dict
 -- ----------------------------
 DROP TABLE IF EXISTS `dict`;
-CREATE TABLE `dict`
-(
-  `id`   varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+CREATE TABLE `dict`  (
+  `id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -145,19 +120,16 @@ CREATE TABLE `dict`
 -- ----------------------------
 -- Records of dict
 -- ----------------------------
-INSERT INTO `dict`
-VALUES ('143534634634', '书籍1');
-INSERT INTO `dict`
-VALUES ('1435346346342', '书籍2');
+INSERT INTO `dict` VALUES ('143534634634', '书籍1');
+INSERT INTO `dict` VALUES ('1435346346342', '书籍2');
 
 -- ----------------------------
 -- Table structure for plan
 -- ----------------------------
 DROP TABLE IF EXISTS `plan`;
-CREATE TABLE `plan`
-(
-  `id`         varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `plan`  (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `start_time` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -165,29 +137,27 @@ CREATE TABLE `plan`
 -- ----------------------------
 -- Records of plan
 -- ----------------------------
-INSERT INTO `plan`
-VALUES ('1312312', '一', '2022-09-14 15:43:48');
+INSERT INTO `plan` VALUES ('1312312', '一', '2022-09-14 15:43:48');
 
 -- ----------------------------
 -- Table structure for sys_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
-CREATE TABLE `sys_menu`
-(
-  `id`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `component`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `create_by`   bigint(20) NULL DEFAULT NULL,
+CREATE TABLE `sys_menu`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_by` bigint(20) NULL DEFAULT NULL,
   `create_time` datetime(6) NULL DEFAULT NULL,
-  `del_flag`    int(11) NULL DEFAULT NULL,
-  `icon`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `menu_name`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `path`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `perms`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `status`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `update_by`   bigint(20) NULL DEFAULT NULL,
+  `del_flag` int(11) NULL DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `menu_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `perms` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `update_by` bigint(20) NULL DEFAULT NULL,
   `update_time` datetime(6) NULL DEFAULT NULL,
-  `visible`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `visible` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
@@ -199,8 +169,7 @@ CREATE TABLE `sys_menu`
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role`
-(
+CREATE TABLE `sys_role`  (
   `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -213,8 +182,7 @@ CREATE TABLE `sys_role`
 -- Table structure for sys_role_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_menu`;
-CREATE TABLE `sys_role_menu`
-(
+CREATE TABLE `sys_role_menu`  (
   `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -227,40 +195,38 @@ CREATE TABLE `sys_role_menu`
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`
-(
-  `id`           varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `avatar`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `create_by`    bigint(20) NULL DEFAULT NULL,
-  `create_time`  datetime(6) NULL DEFAULT NULL,
-  `del_flag`     int(11) NULL DEFAULT NULL,
-  `email`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `nick_name`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `password`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+CREATE TABLE `sys_user`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_by` bigint(20) NULL DEFAULT NULL,
+  `create_time` datetime(6) NULL DEFAULT NULL,
+  `del_flag` int(11) NULL DEFAULT NULL,
+  `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `phone_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `sex`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `status`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `update_by`    bigint(20) NULL DEFAULT NULL,
-  `update_time`  datetime(6) NULL DEFAULT NULL,
-  `username`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `user_type`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `user_name`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `update_by` bigint(20) NULL DEFAULT NULL,
+  `update_time` datetime(6) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user`
-VALUES ('111', '1', 1, '2023-05-16 15:48:00.000000', NULL, '1', '1', '1', '1', '1', '1', 1,
-        '2023-05-16 15:48:24.000000', '1', '1', '111');
+INSERT INTO `sys_user` VALUES ('111', '1', 1, '2023-05-16 15:48:00.000000', NULL, 'dasdas', '1', '1', 'c9f6f9d03f3f55d8b11eb0eb7174f5f4', '1', '1', '1', 1, '2023-05-16 15:48:24.000000', 'larry', '1', '111', NULL);
 
 -- ----------------------------
 -- Table structure for sys_user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_role`;
-CREATE TABLE `sys_user_role`
-(
+CREATE TABLE `sys_user_role`  (
   `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -269,5 +235,4 @@ CREATE TABLE `sys_user_role`
 -- Records of sys_user_role
 -- ----------------------------
 
-SET
-FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
