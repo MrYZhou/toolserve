09c2ffb21925cf72d30f056e93a0cd18170d300b
larry <lingeng@yinmaisoft.com>
2023-06-26 17:44:30 +0800

    修改为从service获取

--- security-module/src/main/java/com/lar/security/outh/server/UserServiceImpl.java
+++ security-module/src/main/java/com/lar/security/outh/server/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.lar.security.outh.server;
 
-import com.lar.security.outh.repository.UserEntity;
+import com.lar.security.outh.model.UserView;
+import org.noear.wood.DbContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 //服务层实现,这边就可以使用具体仓库组织代码
 @Service
 public class UserServiceImpl implements UserService {
 
 
+    @Autowired
+    private DbContext dbContext;
+
     @Override
-    public UserEntity getUserByUserName(String username) {
-        return null;
+    public UserView getUserByUserName(String username) {
+        try {
+            return dbContext.sql("select * from sys_user where username=?", username).getItem(UserView.class);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
