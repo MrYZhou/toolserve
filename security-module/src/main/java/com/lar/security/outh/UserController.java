19deb4f66dbd4dc940f8f5556b8cd0b5da40adf2
larry <lingeng@yinmaisoft.com>
2023-06-26 17:09:47 +0800

    登录接口修改

--- security-module/src/main/java/com/lar/security/outh/UserController.java
+++ security-module/src/main/java/com/lar/security/outh/UserController.java
@@ -2,22 +2,25 @@ package com.lar.security.outh;
 
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
-import com.lar.security.outh.model.UserLogin;
 import com.lar.security.outh.model.UserQuery;
+import com.lar.security.outh.model.UserView;
 import com.lar.security.outh.server.UserService;
+import com.lar.security.outh.util.PasswordUtil;
 import com.lar.util.RedisUtil;
 import com.lar.vo.AppResult;
 import lombok.extern.slf4j.Slf4j;
+import org.noear.wood.DbContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/user" )
+@RequestMapping("/user")
 @Slf4j
 public class UserController {
     @Autowired
@@ -26,42 +29,52 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private DbContext dbContext;
 
-    @PostMapping("/info" )
+    @PostMapping("/info")
     public AppResult<Object> getUser(@RequestBody UserQuery userQuery) {
         return AppResult.success(null);
     }
 
     // 测试登录，浏览器访问： http://localhost:8081/user/login?username=zhang&password=123456
-    @PostMapping("/login" )
-    public AppResult<Object> doLogin(@RequestBody UserLogin user) {
-        // 从数据库拿
-        String encrptPassword = user.getPassword();
-        if ("zhang".equals(user.getUsername()) && "123456".equals(user.getPassword())) {
-            StpUtil.login(10001);
+    @PostMapping("/login")
+    public AppResult<Object> doLogin(@RequestBody UserView user) throws SQLException {
+        UserView userView = dbContext.sql("select * from sys_user where username=?",
+                        user.getUsername())
+                .getItem(UserView.class);
+        String encrptPassword = PasswordUtil.encode(user.getPassword(), userView.getSalt());
+        if (userView != null && encrptPassword.equals(userView.getPassword())) {
+            StpUtil.login(userView.getId());
             String token = StpUtil.getTokenValue();
             HashMap<String, String> map = new HashMap<>();
             map.put("token", token);
             return AppResult.success(map);
         }
-        return AppResult.fail("登录失败" );
+        return AppResult.fail("登录失败");
+    }
+
+    @PostMapping("/registe")
+    public AppResult<Object> registe(@RequestBody UserView user) throws SQLException {
+
+        return AppResult.success();
     }
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
-    @RequestMapping("isLogin" )
+    @RequestMapping("isLogin")
     public String isLogin() {
 
         return "当前会话是否登录：" + StpUtil.isLogin();
     }
 
     // 查询 Token 信息  ---- http://localhost:8081/user/tokenInfo
-    @RequestMapping("tokenInfo" )
+    @RequestMapping("tokenInfo")
     public SaResult tokenInfo() {
         return SaResult.data(StpUtil.getTokenInfo());
     }
 
     // 测试注销  ---- http://localhost:8081/user/logout
-    @RequestMapping("logout" )
+    @RequestMapping("logout")
     public SaResult logout() {
         StpUtil.logout();
         log.info("注销操作");
