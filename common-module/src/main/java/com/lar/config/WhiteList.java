4bdb4dd4df3ee5f65dd6e26287c4d730e347cbab
lingen <1762861715@qq.com>
2023-06-27 14:51:50 +0800

    路由拦截鉴权

---
+++ common-module/src/main/java/com/lar/config/WhiteList.java
@@ -0,0 +1,9 @@
+package com.lar.config;
+
+import java.util.ArrayList;
+
+public class WhiteList {
+    public static final ArrayList<String> url = new ArrayList<>() {{
+        add("/user/login");
+    }};
+}
