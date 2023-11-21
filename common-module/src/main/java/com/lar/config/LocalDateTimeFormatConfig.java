a882e18e83b526aea70e1fcc1dd2ec3f789e0050
lg <1762861715@qq.com>
2023-11-21 18:38:33 +0800

    测试日期

--- common-module/src/main/java/com/lar/config/LocalDateTimeFormatConfig.java
+++ common-module/src/main/java/com/lar/config/LocalDateTimeFormatConfig.java
@@ -3,8 +3,12 @@ package com.lar.config;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -12,10 +16,16 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.format.Formatter;
+import org.springframework.format.datetime.DateFormatter;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 @Configuration
 public class LocalDateTimeFormatConfig {
@@ -26,7 +36,7 @@ public class LocalDateTimeFormatConfig {
     /**
      * 序列化
      */
-    public static class LocalDateTimeSerializerNumLocalDateTimeSerializerNum extends JsonSerializer<LocalDateTime> {
+    public static class LocalDateTimeSerializerNum extends JsonSerializer<LocalDateTime> {
         @Override
         public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers)
                 throws IOException {
@@ -36,8 +46,26 @@ public class LocalDateTimeFormatConfig {
             }
         }
     }
+    public class DateFormatterSerializer extends JsonSerializer<Object> implements Formatter<Date> {
+        private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    /**
+
+        @Override
+        public Date parse(String s, Locale locale) throws ParseException {
+            return dateFormat.parse(s);
+        }
+
+        @Override
+        public String print(Date date, Locale locale) {
+            return dateFormat.format(date);
+        }
+
+        @Override
+        public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+
+        }
+    }
+        /**
      * 反序列化
      */
     public static class LocalDateTimeDeserializerNum extends JsonDeserializer<LocalDateTime> {
@@ -60,16 +88,14 @@ public class LocalDateTimeFormatConfig {
         JavaTimeModule javaTimeModule = new JavaTimeModule();
 //        序列化给前端的显示,自动处理成对应的字符串日期
         javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
-
-        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
-//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
 
-        //接收前端的字符串日期或者时间戳日期
         javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializerNum());
-
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
+//        javaTimeModule.addDeserializer(Date.class, new DateDeserializers.DateDeserializer() );
+
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
