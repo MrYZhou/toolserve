77c25ca7978d5e30b84abe98977972eca3547454
lg <1762861715@qq.com>
2023-11-22 10:07:44 +0800

    增加date对象的转换

--- common-module/src/main/java/com/lar/config/LocalDateTimeFormatConfig.java
+++ common-module/src/main/java/com/lar/config/LocalDateTimeFormatConfig.java
@@ -1,11 +1,13 @@
 package com.lar.config;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -16,16 +18,14 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.format.Formatter;
-import org.springframework.format.datetime.DateFormatter;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Locale;
 
 @Configuration
 public class LocalDateTimeFormatConfig {
@@ -33,72 +33,46 @@ public class LocalDateTimeFormatConfig {
     private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
     private static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
 
-    /**
-     * 序列化
-     */
-    public static class LocalDateTimeSerializerNum extends JsonSerializer<LocalDateTime> {
-        @Override
-        public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers)
-                throws IOException {
-            if (value != null) {
-                long timestamp = value.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-                gen.writeNumber(timestamp);
-            }
-        }
-    }
-    public class DateFormatterSerializer extends JsonSerializer<Object> implements Formatter<Date> {
-        private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-
-        @Override
-        public Date parse(String s, Locale locale) throws ParseException {
-            return dateFormat.parse(s);
-        }
-
-        @Override
-        public String print(Date date, Locale locale) {
-            return dateFormat.format(date);
-        }
-
-        @Override
-        public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-
-        }
-    }
-        /**
-     * 反序列化
-     */
-    public static class LocalDateTimeDeserializerNum extends JsonDeserializer<LocalDateTime> {
-        @Override
-        public LocalDateTime deserialize(JsonParser p, DeserializationContext deserializationContext)
-                throws IOException {
-            long timestamp = p.getValueAsLong();
-            if (timestamp > 0) {
-                return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
-            } else {
-                return null;
-            }
-        }
-    }
-
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
+        // 序列化为时间字符串给前端,和前端时间字符串反序列化为日期对象
         ObjectMapper objectMapper = new ObjectMapper();
         JavaTimeModule javaTimeModule = new JavaTimeModule();
-//        序列化给前端的显示,自动处理成对应的字符串日期
+
         javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
         javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
-
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializerNum());
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
         javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
         javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
-//        javaTimeModule.addDeserializer(Date.class, new DateDeserializers.DateDeserializer() );
+
+        javaTimeModule.addSerializer(Date.class, new DateSerializer());
+        javaTimeModule.addDeserializer(Date.class, new DateDeserializer());
 
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
 
+    public static class DateSerializer extends JsonSerializer<Date> {
+        @Override
+        public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            SimpleDateFormat formatter = new SimpleDateFormat(DEFAULT_DATE_TIME_PATTERN);
+            String formattedDate = formatter.format(date);
+            jsonGenerator.writeString(formattedDate);
+        }
+    }
+
+    public static class DateDeserializer extends JsonDeserializer<Date> {
+        @Override
+        public Date deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException, JacksonException {
+            long timestamp = p.getValueAsLong();
+            if (timestamp > 0) {
+                return new Date(timestamp);
+            } else {
+                return null;
+            }
+        }
+    }
 
 }
