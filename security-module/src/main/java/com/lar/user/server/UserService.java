a001aa52f480101909b7fa59a1147c1471ad3d29
larry <1762861715@qq.com>
2023-12-07 17:05:00 +0800

    注册接口

--- security-module/src/main/java/com/lar/user/server/UserService.java
+++ security-module/src/main/java/com/lar/user/server/UserService.java
@@ -3,7 +3,11 @@ package com.lar.user.server;
 
 import com.lar.user.repository.UserEntity;
 
+import java.sql.SQLException;
+
 //对复杂应用可以使用，如调用超过4个服务或仓库领域,否则这一层可以空置
 public interface UserService {
     UserEntity getUserByUserName(String username);
+
+    Long insertUser(UserEntity userEntity) throws SQLException;
 }
