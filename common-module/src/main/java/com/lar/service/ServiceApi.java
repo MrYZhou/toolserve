d37e1cacfe7623b1c168bc9abdf91b0860cfd8f4
lg <1762861715@qq.com>
2023-12-25 10:40:01 +0800

    boot依赖升级3.2.1

--- common-module/src/main/java/com/lar/service/ServiceApi.java
+++ common-module/src/main/java/com/lar/service/ServiceApi.java
@@ -5,5 +5,6 @@ import org.noear.dami.spring.boot.annotation.DamiTopic;
 @DamiTopic("toolserve.event")
 public interface ServiceApi {
     void reload(String name);
-     void test2(String name);
+
+    void test2(String name);
 }
