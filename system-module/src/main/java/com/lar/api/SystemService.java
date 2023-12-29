465e93631068836fcdf8443c7e9250df07df69b7
lg <1762861715@qq.com>
2023-12-29 10:15:48 +0800

    调整api结构

--- system-module/src/main/java/com/lar/api/SystemService.java
+++ system-module/src/main/java/com/lar/api/SystemService.java
@@ -2,7 +2,10 @@ package com.lar.api;
 
 import org.noear.dami.spring.boot.annotation.DamiTopic;
 
-@DamiTopic("toolserve.event")
+/**
+ * 提供common的api层的实现，是一一对应的
+ */
+@DamiTopic("toolserve.systemEvent")
 public class SystemService {
     public void test2(String name){
         System.out.println("reload system:"+name);
