46dcd6b76a033cf04088cce82f111af95946b77e
larry <larry>
2024-01-23 21:20:20 +0800

    satoken二级校验

--- security-module/src/main/java/com/lar/oauth/AuthController.java
+++ security-module/src/main/java/com/lar/oauth/AuthController.java
@@ -1,6 +1,7 @@
 package com.lar.oauth;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckSafe;
 import cn.dev33.satoken.annotation.SaIgnore;
 import cn.dev33.satoken.stp.SaLoginModel;
 import cn.dev33.satoken.stp.StpUtil;
@@ -62,11 +63,11 @@ public class AuthController {
 
         // 指定token有效期(单位: 秒)，如下所示token七天有效
         StpUtil.login(userEntity.getId(), new SaLoginModel()
-                .setDevice("PC")                 // 此次登录的客户端设备类型, 用于[同端互斥登录]时指定此次登录的设备类型
-                .setIsLastingCookie(true)        // 是否为持久Cookie（临时Cookie在浏览器关闭时会自动删除，持久Cookie在重新打开后依然存在）
-                .setTimeout(60 * 60 * 24 * 7)    // 指定此次登录token的有效期, 单位:秒 （如未指定，自动取全局配置的 timeout 值）
+                        .setDevice("PC")                 // 此次登录的客户端设备类型, 用于[同端互斥登录]时指定此次登录的设备类型
+                        .setIsLastingCookie(true)        // 是否为持久Cookie（临时Cookie在浏览器关闭时会自动删除，持久Cookie在重新打开后依然存在）
+                        .setTimeout(60 * 60 * 24 * 7)    // 指定此次登录token的有效期, 单位:秒 （如未指定，自动取全局配置的 timeout 值）
 //                .setToken("xxxx-xxxx-xxxx-xxxx") // 预定此次登录的生成的Token
-                .setIsWriteHeader(false)         // 是否在登录后将 Token 写入到响应头
+                        .setIsWriteHeader(false)         // 是否在登录后将 Token 写入到响应头
         );
         String token = StpUtil.getTokenValue();
 
@@ -268,5 +269,82 @@ public class AuthController {
 
         return SaResult.ok();
     }
+
+    // 删除仓库    ---- http://localhost:8081/safe/deleteProject
+    @RequestMapping("deleteProject")
+    public SaResult deleteProject(String projectId) {
+        // 第1步，先检查当前会话是否已完成二级认证
+        // 		这个地方既可以通过 StpUtil.isSafe() 手动判断，
+        // 		也可以通过 StpUtil.checkSafe() 或者 @SaCheckSafe 来校验（校验不通过时将抛出 NotSafeException 异常）
+        if (!StpUtil.isSafe()) {
+            return SaResult.error("仓库删除失败，请完成二级认证后再次访问接口");
+        }
+
+        // 第2步，如果已完成二级认证，则开始执行业务逻辑
+        // ...
+
+        // 第3步，返回结果
+        return SaResult.ok("仓库删除成功");
+    }
+
+    // 提供密码进行二级认证    ---- http://localhost:8081/safe/openSafe?password=123456
+    @RequestMapping("openSafe")
+    public SaResult openSafe(String password) {
+        // 比对密码（此处只是举例，真实项目时可拿其它参数进行校验）
+        if ("123456".equals(password)) {
+
+            // 比对成功，为当前会话打开二级认证，有效期为120秒，意为在120秒内再调用 deleteProject 接口都无需提供密码
+            StpUtil.openSafe(120);
+            return SaResult.ok("二级认证成功");
+        }
+
+        // 如果密码校验失败，则二级认证也会失败
+        return SaResult.error("二级认证失败");
+    }
+
+    // 手动关闭二级认证    ---- http://localhost:8081/safe/closeSafe
+    @RequestMapping("closeSafe")
+    public SaResult closeSafe() {
+        StpUtil.closeSafe();
+        return SaResult.ok();
+    }
+
+
+    // ------------------ 指定业务类型进行二级认证
+
+    // 获取应用秘钥    ---- http://localhost:8081/safe/getClientSecret
+    @RequestMapping("getClientSecret")
+    @SaCheckSafe(value="client")
+    public SaResult getClientSecret() {
+        // 第1步，先检查当前会话是否已完成 client业务 的二级认证
+        StpUtil.checkSafe("client");
+
+        // 第2步，如果已完成二级认证，则返回数据
+        return SaResult.data("aaaa-bbbb-cccc-dddd-eeee");
+    }
+
+    // 提供手势密码进行二级认证    ---- http://localhost:8081/safe/openClientSafe?gesture=35789
+    @RequestMapping("openClientSafe")
+    public SaResult openClientSafe(String gesture) {
+        // 比对手势密码（此处只是举例，真实项目时可拿其它参数进行校验）
+        if("35789".equals(gesture)) {
+
+            // 比对成功，为当前会话打开二级认证：
+            // 业务类型为：client
+            // 有效期为600秒==10分钟，意为在10分钟内，调用 getClientSecret 时都无需再提供手势密码
+            StpUtil.openSafe("client", 600);
+            return SaResult.ok("二级认证成功");
+        }
+
+        // 如果密码校验失败，则二级认证也会失败
+        return SaResult.error("二级认证失败");
+    }
+
+    // 查询当前会话是否已完成指定的二级认证    ---- http://localhost:8081/safe/isClientSafe
+    @RequestMapping("isClientSafe")
+    public SaResult isClientSafe() {
+        return SaResult.ok("当前是否已完成 client 二级认证：" + StpUtil.isSafe("client"));
+    }
+
 
 }
