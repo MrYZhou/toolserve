37af65283ca3e938476214bb6e6a84ffca25910c
larry <larry>
2024-01-26 10:19:25 +0800

    多账号体系自定义配置和token标识修改来支持一个账号在一个体系下多登录

--- security-module/src/main/java/com/lar/config/StpCustomUtil.java
+++ security-module/src/main/java/com/lar/config/StpCustomUtil.java
@@ -1,9 +1,35 @@
 package com.lar.config;
 
+import cn.dev33.satoken.config.SaTokenConfig;
+import cn.dev33.satoken.stp.StpLogic;
+
 public class StpCustomUtil {
 
     /**
      * 账号体系标识,用于在一套系统实现多套账号体系
      */
     public static final String TYPE = "custom";
+
+    // 使用匿名子类 重写`stpLogic对象`的一些方法
+    public static StpLogic stpLogic = new StpLogic("custom") {
+
+        // 首先自定义一个 Config 对象
+        SaTokenConfig config = new SaTokenConfig()
+                .setTokenName("satoken")
+                .setTimeout(2592000)
+                // ... 其它set
+                ;
+
+        // 然后重写 stpLogic 配置获取方法
+        @Override
+        public SaTokenConfig getConfig() {
+            return config;
+        }
+
+        // 重写 StpLogic 类下的 `splicingKeyTokenName` 函数，返回一个与 `StpUtil` 不同的token名称, 防止冲突
+        @Override
+        public String splicingKeyTokenName() {
+            return super.splicingKeyTokenName() + "-custom";
+        }
+    };
 }
