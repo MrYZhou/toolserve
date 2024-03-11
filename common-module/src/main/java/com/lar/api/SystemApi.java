c61acc88e0e206b81496a1f720ea32fa6bce6e1a
larry <larry>
2024-03-11 18:35:34 +0800

    nami

--- common-module/src/main/java/com/lar/api/SystemApi.java
+++ common-module/src/main/java/com/lar/api/SystemApi.java
@@ -4,7 +4,6 @@ import org.noear.dami.spring.boot.annotation.DamiTopic;
 
 @DamiTopic("toolserve.systemEvent")
 public interface SystemApi {
-    void reload(String name);
 
-    void test2(String name);
+    void testService(String name);
 }
--- system-module/src/main/java/com/lar/api/SystemService.java
+++ system-module/src/main/java/com/lar/api/SystemService.java
@@ -7,7 +7,7 @@ import org.noear.dami.spring.boot.annotation.DamiTopic;
  */
 @DamiTopic("toolserve.systemEvent")
 public class SystemService {
-    public void test2(String name){
+    public void testService(String name){
         System.out.println("reload system:"+name);
     }
 
