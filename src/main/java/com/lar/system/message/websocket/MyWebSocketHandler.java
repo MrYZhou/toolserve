a0e9cf2e2eec76de13c0dd759db69d06b3785ae3
lg <1762861715@qq.com>
2024-08-20 10:31:45 +0800

    1

--- src/main/java/com/lar/system/message/websocket/MyWebSocketHandler.java
+++ src/main/java/com/lar/system/message/websocket/MyWebSocketHandler.java
@@ -1,4 +1,4 @@
-package com.lar.system.message.websocketNew;
+package com.lar.system.message.websocket;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
