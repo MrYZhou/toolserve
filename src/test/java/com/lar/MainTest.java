c8b872f860db391b37e127535b2fcf7ccfcbd9f5
lg <1762861715@qq.com>
2024-08-20 11:07:15 +0800

    1

--- src/test/java/com/lar/MainTest.java
+++ src/test/java/com/lar/MainTest.java
@@ -1,9 +1,6 @@
 package com.lar;
 
 import org.junit.jupiter.api.Test;
-import org.noear.folkmq.FolkMQ;
-import org.noear.folkmq.client.MqClient;
-import org.noear.folkmq.client.MqMessage;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.StopWatch;
 
@@ -22,19 +19,6 @@ public class MainTest {
         System.out.println(stopWatch.getTotalTimeMillis());
     }
 
-    @Test
-    public void testfolk() throws Exception {
-        //创建客户端，并连接
-        MqClient client = FolkMQ.createClient("folkmq://127.0.0.1:18602").connect();
-
-        //订阅主题，并指定加入的消费者分组
-        client.subscribe("demo", "demoapp", message -> {
-            System.out.println("========================================");
-            System.out.println(message);
-        });
-
-        //发布消息
-        client.publishAsync("demo", new MqMessage("hello world!"));
-    }
+    
 
 }
