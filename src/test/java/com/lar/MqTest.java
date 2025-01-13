f87cfeb3ae121b7c995cb561ad6f5b9f71817deb
lg <1762861715@qq.com>
2025-01-13 18:07:24 +0800

    mq基础使用

---
+++ src/test/java/com/lar/MqTest.java
@@ -0,0 +1,25 @@
+package com.lar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+@SpringBootTest
+public class MqTest {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Test
+    public void testSimpleQueue() {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, spring amqp!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+
+
+}
