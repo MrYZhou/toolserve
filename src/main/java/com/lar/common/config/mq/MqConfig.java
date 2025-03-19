18b0d6d2e3f6fd0b1cbb2a961a03e8eb53dae2e7
larry <1762861715@qq.com>
2025-03-19 17:47:02 +0800

    mq优化，版本升级

--- src/main/java/com/lar/common/config/mq/MqConfig.java
+++ src/main/java/com/lar/common/config/mq/MqConfig.java
@@ -1,14 +1,80 @@
 package com.lar.common.config.mq;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MqConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    // 定义队列名称
+    public static final String QUEUE_NAME = "myQueue";
+    // 定义交换机名称
+    public static final String EXCHANGE_NAME = "myExchange";
+    // 定义路由键
+    public static final String ROUTING_KEY = "myRoutingKey";
+
+    // 创建连接工厂
     @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter(){
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualHost);
+        return connectionFactory;
+    }
+
+    // 创建RabbitTemplate
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    // 创建消息转换器
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    // 创建队列
+    @Bean
+    public Queue myQueue() {
+        return new Queue(QUEUE_NAME, true); // true表示持久化队列
+    }
+
+    // 创建交换机
+    @Bean
+    public DirectExchange myExchange() {
+        return new DirectExchange(EXCHANGE_NAME, true, false); // true表示持久化交换机
+    }
+
+    // 绑定队列和交换机
+    @Bean
+    public Binding binding(Queue myQueue, DirectExchange myExchange) {
+        return BindingBuilder.bind(myQueue).to(myExchange).with(ROUTING_KEY);
+    }
 }
